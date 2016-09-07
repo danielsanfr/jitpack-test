3f66c472f5839e8550476dee49432e8db1cff03f
Daniel San <daniel.samrocha@gmail.com>
2016-09-07 19:47:01 -0300

    Added documentation to test javadoc generation
    Signed-off-by: Daniel San <daniel.samrocha@gmail.com>

--- moduleone/src/main/java/br/com/danielsan/moduleone/Addition.java
+++ moduleone/src/main/java/br/com/danielsan/moduleone/Addition.java
@@ -5,8 +5,17 @@ package br.com.danielsan.moduleone;
  */
 public final class Addition {
 
+    /**
+     * Constructor
+     */
     private Addition() { }
 
+    /**
+     * Make addition
+     * @param a first argument of the operation
+     * @param b second argument of the operation
+     * @return the addition (a + b)
+     */
     public static int add(int a, int b) {
         return a + b;
     }
--- moduletwo/src/main/java/br/com/danielsan/moduletwo/Subtraction.java
+++ moduletwo/src/main/java/br/com/danielsan/moduletwo/Subtraction.java
@@ -5,8 +5,17 @@ package br.com.danielsan.moduletwo;
  */
 public final class Subtraction {
 
+    /**
+     * Constructor
+     */
     private Subtraction() { }
 
+    /**
+     * Make subtraction
+     * @param a first argument of the operation
+     * @param b second argument of the operation
+     * @return the subtraction (a - b)
+     */
     public static int sub(int a, int b) {
         return a - b;
     }
